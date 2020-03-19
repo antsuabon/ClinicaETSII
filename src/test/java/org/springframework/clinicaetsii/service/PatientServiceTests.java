5522026ff1be82632586d67a5a45ad03d4f57573
José <Jos@DESKTOP-VHHEN05>
2020-03-19 12:26:36 +0100

    error corercted

--- src/test/java/org/springframework/clinicaetsii/service/PatientServiceTests.java
+++ src/test/java/org/springframework/clinicaetsii/service/PatientServiceTests.java
@@ -5,32 +5,30 @@ import java.util.Collection;
 import java.util.List;
 
 import org.assertj.core.api.Assertions;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.clinicaetsii.model.Doctor;
-import org.springframework.clinicaetsii.model.Service;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Service;
 
 @DataJpaTest(includeFilters = @ComponentScan.Filter(Service.class))
 public class PatientServiceTests {
 
-	@Autowired
-	protected PatientService patientService;
-	
 	@Autowired
 	protected DoctorService doctorService;
 
 
 	@Test
 	void shouldListDoctorsServices() {
+		
 		Collection<Doctor> doctors = this.doctorService.findAllDoctors();
 
 		List<Doctor> listDoctors = new ArrayList<>(doctors);
 		boolean all = listDoctors.size() == 3;
 		
 		
-		Assertions.assertThat(all).isEqualTo(true);
+		Assertions.assertThat(all);
 
 	}
 	
