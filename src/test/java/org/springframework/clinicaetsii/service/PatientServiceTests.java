eb8ea90e3b15bc25eba4fd1c60923fbef851f876
aleja <aleja@DESKTOP-IKB2RIB>
2020-03-21 18:21:41 +0100

    PU-005: Unfinished
    Quedan las pruebas de los controladores

--- src/test/java/org/springframework/clinicaetsii/service/PatientServiceTests.java
+++ src/test/java/org/springframework/clinicaetsii/service/PatientServiceTests.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.clinicaetsii.model.Doctor;
+import org.springframework.clinicaetsii.model.Patient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +18,9 @@ public class PatientServiceTests {
 
 	@Autowired
 	protected DoctorService doctorService;
+	
+	@Autowired
+	protected PatientService patientService;
 
 
 	@Test
@@ -30,6 +34,24 @@ public class PatientServiceTests {
 		
 		Assertions.assertThat(all);
 
+	}
+	
+	@Test
+	void shouldUpdatePatient() {
+		Collection<Patient> patients1 = this.patientService.findPatients();
+		int initSize = patients1.size();
+		
+		Patient patient1 = this.patientService.findPatientById(4);
+		Doctor doctor1 = this.doctorService.findDoctorById(2);
+		
+		patient1.setGeneralPractitioner(doctor1);
+		
+		this.patientService.savePatient(patient1);
+		
+		Collection<Patient> patients2 = this.patientService.findPatients();
+		int finalSize = patients2.size();
+		
+		Assertions.assertThat(initSize).isEqualTo(finalSize);
 	}
 	
 	
