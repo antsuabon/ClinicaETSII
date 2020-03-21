0b1d9f9fe4f5fe935ccc602b65f0202d4dd2d9b7
Pablo <Pablo@LAPTOP-548JJDPI>
2020-03-21 11:34:05 +0100

    PU-008 AppointmentValidatorTest sin los controladores #56

--- src/main/java/org/springframework/clinicaetsii/web/validator/AppointmentValidator.java
+++ src/main/java/org/springframework/clinicaetsii/web/validator/AppointmentValidator.java
@@ -14,16 +14,14 @@ public class AppointmentValidator implements Validator {
 
 	@Override
 	public void validate(final Object target, final Errors errors) {
+
 		Appointment appointment = (Appointment) target;
 
 		if (appointment.getStartTime() == null) {
 			errors.rejectValue("startTime", "no_startTime", "La fecha de inicio de la cita no puede ser nula");
-		}
-		if (appointment.getEndTime() == null) {
+		} else if (appointment.getEndTime() == null) {
 			errors.rejectValue("endTime", "no_endTime", "La fecha de fin de la cita no puede ser nula");
-		}
-
-		if (!appointment.getStartTime().equals(appointment.getEndTime().minusMinutes(7))) {
+		} else if (!appointment.getStartTime().equals(appointment.getEndTime().minusMinutes(7))) {
 			errors.rejectValue("endTime", "over_minutes", "La diferecia entre en comienzo y fin de la cita debe de ser de 7 minutos");
 		}
 
