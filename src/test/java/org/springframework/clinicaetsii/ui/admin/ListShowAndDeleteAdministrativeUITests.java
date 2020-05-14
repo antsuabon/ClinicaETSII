3fc7189491c7ed3b6d7bb65b277ac2c203b4583c
Alejandro Sánchez Saavedra <alesansaa@alum.us.es>
2020-05-14 18:20:30 +0200

    IU-023
    Done

---
+++ src/test/java/org/springframework/clinicaetsii/ui/admin/ListShowAndDeleteAdministrativeUITests.java
@@ -0,0 +1,98 @@
+package org.springframework.clinicaetsii.ui.admin;
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = ClassMode.AFTER_CLASS)
+public class ListShowAndDeleteAdministrativeUITests {
+	
+  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @BeforeEach
+  public void setUp() throws Exception {
+	String pathToGeckoDriver = "C:\\Users\\aleja\\Desktop\\universidad\\gecko";
+	System.setProperty("webdriver.gecko.driver", pathToGeckoDriver + "\\geckodriver.exe");
+    driver = new FirefoxDriver();
+    baseUrl = "https://www.google.com/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void listShowAndDeleteAdministrative() throws Exception {
+    driver.get("http://localhost:9090/");
+    driver.findElement(By.xpath("//a[contains(text(),'Iniciar sesión')]")).click();
+    driver.findElement(By.id("username")).click();
+    driver.findElement(By.id("username")).clear();
+    driver.findElement(By.id("username")).sendKeys("admin");
+    driver.findElement(By.id("password")).click();
+    driver.findElement(By.id("password")).clear();
+    driver.findElement(By.id("password")).sendKeys("admin");
+    driver.findElement(By.xpath("//button[@type='submit']")).click();
+    driver.findElement(By.xpath("//a[contains(text(),'Administrador')]")).click();
+    driver.findElement(By.xpath("//a[contains(@href, '/admin/administratives')]")).click();
+    driver.findElement(By.xpath("//a[contains(text(),'Seleccionar Administrativo')]")).click();
+    driver.findElement(By.xpath("//a[contains(text(),'Eliminar Administrativo')]")).click();
+    assertFalse(isElementPresent(By.xpath("//table[@id='administrativesTable']/tbody/tr[4]/td")));
+  }
+
+  @AfterEach
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }
+}
